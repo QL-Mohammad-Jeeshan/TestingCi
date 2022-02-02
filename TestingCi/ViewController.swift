c4c90f051355f4c74e0262be07e3b41b63bff8e8
MjSiddique <98814613+MjSiddique@users.noreply.github.com>
2022-02-02 12:04:51 +0530

    Add files via upload

---
+++ TestingCi/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestingCi
+//
+//  Created by Mohammed Jeeshan on 20/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func BtnClick(_ sender: UIButton) {
+    print("Its working Bro")
+  }
+  
+}
+
