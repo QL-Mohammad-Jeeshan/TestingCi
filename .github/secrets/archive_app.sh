b1e868197dd248b84d0301a2fc088c07f1f9719b
Mohammad Jeeshan <Mohammad.jeeshan@quokkalabs.com>
2022-02-16 16:19:12 +0530

    Today Attempt-20

--- .github/secrets/archive_app.sh
+++ .github/secrets/archive_app.sh
@@ -1,8 +1,8 @@
 set -eo pipefail
 
-xcodebuild -workspace TestingCi.xcworkspace \
-            -scheme TestingCi\ iOS \
-            -sdk iphoneos \
-            -configuration AppStoreDistribution \
-            -archivePath $PWD/build/TestingCi.xcarchive \
-            clean archive | xcpretty
+ xcodebuild  -workspace TestingCi.xcworkspace \
+             -scheme TestingCi \
+             -sdk iphoneos \
+             -configuration Debug \
+             -archivePath $PWD/build/TestingCi.xcarchive \
+             clean archive | xcpretty
