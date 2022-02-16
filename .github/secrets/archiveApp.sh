7fcf8b8292833782376f06fae17b54b82ba0bafa
MjSiddique <98814613+MjSiddique@users.noreply.github.com>
2022-02-16 16:25:29 +0530

    Create archiveApp.sh

---
+++ .github/secrets/archiveApp.sh
@@ -0,0 +1,8 @@
+set -eo pipefail
+
+ xcodebuild  -workspace TestingCi.xcworkspace \
+             -scheme TestingCi \
+             -sdk iphoneos \
+             -configuration Debug \
+             -archivePath $PWD/build/TestingCi.xcarchive \
+             clean archive | xcpretty
