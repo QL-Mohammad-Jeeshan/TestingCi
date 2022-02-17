645b531c3f90c36825d1b57ee8ca889479909d61
Mohammad Jeeshan <Mohammad.jeeshan@quokkalabs.com>
2022-02-17 14:00:56 +0530

    Today Attempt-22

---
+++ Pods/Target Support Files/Pods-TestingCi-TestingCiUITests/Pods-TestingCi-TestingCiUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestingCi_TestingCiUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TestingCi_TestingCiUITests
+@end
---
+++ Pods/Target Support Files/Pods-TestingCi/Pods-TestingCi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestingCi : NSObject
+@end
+@implementation PodsDummy_Pods_TestingCi
+@end
